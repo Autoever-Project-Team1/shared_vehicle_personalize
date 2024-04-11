4c2da0891afd9be3b25d8625e71af057ff674626
woojinrnd <woojin0342@gmail.com>
2024-04-11 18:17:01 +0900

    feat : add lcd drvier
    lcd drvier and config file for stm32

---
+++ 2_rtos/Core/Src/IO/LCD/I2C_LCD_cfg.c
@@ -0,0 +1,15 @@
+#include "IO/LCD/I2C_LCD_cfg.h"
+#include "IO/LCD/I2C_LCD.h"
+
+extern I2C_HandleTypeDef hi2c2;
+
+const I2C_LCD_CfgType I2C_LCD_CfgParam[I2C_LCD_MAX] =
+{
+	{   /*  Configuration Parameter For I2C_LCD Instance #1   */
+		I2C_LCD_1,	/* Index of I2C_LCD Instance #1           */
+		&hi2c2,		/* Hardware I2C Module's Handle           */
+		0x27,		/* Hardware I2C_LCD Device Address        */
+		16,			/* LCD Columns Count                      */
+		3			/* LCD Rows Count                         */
+	}
+};
